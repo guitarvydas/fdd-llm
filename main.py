edea22f00a1da15b491f96ffb053fc6aa9fdefcb
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-05-08 18:14:23 -0400

    LLM_test_jig

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import kernel0d as zd
+
+[palette, env] = zd.initialize_from_files (sys.argv[3:])
+top = zd.start_bare (part_name=sys.argv[2], palette=palette, env=env)
+zd.inject (top, "", sys.argv[1])
+zd.finalize (top)
+
